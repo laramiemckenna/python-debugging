bbbffc847b2aac1846845f815f2c2cbcd9f26618
Laramie Smith <lmakozbek@gmail.com>
2023-02-24 16:40:23 -0600

    submission

--- area_of_rectangle.py
+++ area_of_rectangle.py
@@ -29,7 +29,9 @@ def area_of_rectangle(height, width = None):
     >>> area_of_rectangle (7, 2)
     14
     """
-    if width:
+
+    #I added "is None" to the following line, which allows for the calculation of a square or rectangle by first checking if width is given and then moving to the line "width = height" if it is not. "Width" alone would not be enough to accomplish this task.
+    if width is None:
         width = height
     area = height * width
     return area
@@ -37,19 +39,23 @@ def area_of_rectangle(height, width = None):
 if __name__ == '__main__':
     if (len(sys.argv) < 2) or (len(sys.argv) > 3):
         message = (
-                "{script_name}: Expecting one or two command-line arguments:\n"
-                "\tthe height of a square or the height and width of a "
-                "rectangle".format(script_name = sys.argv[0]))
+            #these lines were not correctly indented
+            "{script_name}: Expecting one or two command-line arguments:\n"
+            "\tthe height of a square or the height and width of a "
+            "rectangle".format(script_name = sys.argv[0]))
         sys.exit(message)
-    height = sys.argv[1]
+    #to address the error from the first test, I turned the height and width variables into integers as they were previously defined as strings.
+    height = int(sys.argv[1])
     width = height
-    if len(sys.argv) > 3:
-        width = sys.argv[1]
+    #the > in the following line needed to be replaced with == to specify that the third command line argument, if it exists, should be used as width
+    if len(sys.argv) == 3:
+        width = int(sys.argv[2])
 
     area = area_of_rectangle(height, width)
 
     message = "The area of a {h} X {w} rectangle is {a}".format(
-            h = height,
-            w = width,
-            a = area)
+        #these variables were not correctly indented
+        h = height,
+        w = width,
+        a = area)
     print(message)
